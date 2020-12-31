26a2473a6aca08666a116047c098a2374925e3d3
ajwu100 <ajwu100@gmail.com>
2020-12-31 01:40:19 -0500

    Completed Leaflet-Step-1

---
+++ Leaflet-Step-1/config.js
@@ -0,0 +1,2 @@
+// API Key for Mapbox
+const API_KEY = "pk.eyJ1IjoiYWp3dTEwMCIsImEiOiJja2lzMzhnb3YwZHViMnVyeGZpNHJiMTZwIn0.MKIMiQD8yXZeE5y2Y2Xnpg";
---
+++ Leaflet-Step-1/logic.js
@@ -0,0 +1,86 @@
+// Store our JSON API endpoint for all past 30 days earthquakes inside usgsUrl variable.
+var usgsUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+console.log(usgsUrl);
+
+
+// Create a map object
+var EQMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 1
+});
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(EQMap);
+
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function (EQMap) {
+    var div = L.DomUtil.create('div', "legend"),
+        mag = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"],
+        labels = ["green", "greenyellow", "yellow", "orange", "orangered", "red"];
+    for (var i = 0; i < mag.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + labels[i] + '"></i> ' +
+            mag[i] + '<br>'
+    }
+    return div;
+};
+
+legend.addTo(EQMap);
+
+// Perform a GET request to the usgsUrl URL.
+d3.json(usgsUrl).then(function (data) {
+    //console.log(data);
+    var earthquakes = data.features;
+    //console.log(earthquakes);
+
+    // Loop through each earthquakes array.  
+    earthquakes.forEach((earthquake) => {
+        const coords = earthquake.geometry.coordinates;
+        //console.log(coords);
+        const lng = coords[0];
+        const lat = coords[1];
+        //console.log(lng);
+        //console.log(lat);
+        const lnglat = { lon: lng, lat: lat };
+        console.log(lnglat);
+
+        // Color each earthquake by magnitude and link radius of each circle marker to magnitude.
+        color = "red";
+        if (earthquake.properties.mag < 1) {
+            color = "green";
+        } else if (earthquake.properties.mag > 1 && earthquake.properties.mag < 2) {
+            color = "greenyellow";
+        } else if (earthquake.properties.mag > 2 && earthquake.properties.mag < 3) {
+            color = "yellow";
+        } else if (earthquake.properties.mag > 3 && earthquake.properties.mag < 4) {
+            color = "orange";
+        } else if (earthquake.properties.mag > 4 && earthquake.properties.mag < 5) {
+            color = "orangered";
+        }
+        // Add circles to map
+        var circle = L.circle(lnglat, {
+            fillOpacity: 0.6,
+            color: "none",
+            fillColor: color,
+            radius: earthquake.properties.mag * 15000,
+        }).bindPopup(
+            `<h1>Place: ${earthquake.properties.place}</h1>
+        <h2>Magnitude: ${earthquake.properties.mag}</h2>
+        <h2>Tsunami: ${earthquake.properties.tsunami}</h2>
+        `);
+        circle.addTo(EQMap);
+    });
+
+
+});
+
+
+
